dbf308ea70ece0159d9c0289baae13fcbd0d309d
Hzy0913 <hanzhaoy@chanjet.com>
2021-05-14 15:21:47 +0800

    style: add type verification

--- src/declare.ts
+++ src/declare.ts
@@ -19,3 +19,28 @@ export interface UpdateSelection {
   id?: string;
   content?: contentType;
 }
+
+export interface TransformXandYType {
+  direction: 'right-bottom' | 'right-top' | 'left-top' | 'left-bottom';
+  width: number;
+  height: number;
+  startX: number;
+  startY: number;
+}
+
+export interface ComputedPositionType {
+  position: { x: number; y: number; width: number; height: number };
+  offsetX: number;
+  offsetY: number;
+  container: { width: number; height: number };
+}
+
+export type directionType = 'left-top' | 'top' | 'right-top' | 'right' | 'left-bottom' |
+  'bottom' | 'right-bottom' | 'left';
+export interface ComputedSizeType {
+  direction: directionType;
+  offset: number;
+  position: number;
+  containerSize?: number;
+  sectionSize?: number;
+}
