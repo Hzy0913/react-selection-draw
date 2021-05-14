6e2377b660c1dddb2ae4fd77bf4ef12bf9aa2ef6
Hzy0913 <hanzhaoy@chanjet.com>
2021-05-14 14:09:44 +0800

    feat: update declare

--- src/declare.ts
+++ src/declare.ts
@@ -1,6 +1,21 @@
+import React from 'react';
+
+type selectionChageType = 'create' | 'update' | 'add' | 'delete' | 'move-start' | 'move-ing' |
+  'move-end' | 'resize-start' | 'resize-ing' | 'resize-end';
+type contentType = { x: number; y: number; width: number; height: number; node?: any };
+type selectionsType = { [id: string]: contentType };
+
+export interface ReactSelectionDrawProps {
+  className?: string;
+  style?: React.CSSProperties;
+  selectionChange?: (type: selectionChageType, selections: selectionsType, id: string) => void;
+  selectionRender?: (id: string) => any;
+  onDelete?: (id: string) => void | Promise<{}>;
+  selectionOnClick?: (id: string, target) => void;
+}
+
 export interface UpdateSelection {
   type: 'add' | 'update' | 'delete';
   id?: string;
-  content?: { x: number; y: number; width: number; height: number; node: any };
+  content?: contentType;
 }
-
