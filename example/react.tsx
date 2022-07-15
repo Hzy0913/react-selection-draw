4c6c73f3359bf2143003e13ce520fa1d019c2295
Hzy0913 <rickyhzy@gmail.com>
2022-07-15 09:56:08 +0800

    feat: update demo

--- example/react.tsx
+++ example/react.tsx
@@ -13,14 +13,51 @@ interface IAppState {
 }
 
 class App extends React.Component<IAppProps, IAppState> {
+  action;
+
   constructor(props) {
     super(props);
+  }
+
+  handle = () => {
+    console.log(this.action.selectionAction({
+      type: 'add',
+      content: {
+        height: 145,
+        width: 112,
+        x: 248,
+        y: 101,
+        node: <h1>1123</h1>
+      }
+    }), 123123)
   }
 
   public render(): JSX.Element {
     return (
       <div className="arc-progress-container">
-        <SelectionCreator />
+        <button onClick={this.handle}>点击</button>
+        <SelectionCreator
+          // minWidth={100}
+          // minHeight={20}
+          ref={ref => this.action = ref}
+          createOperator={(v) => <span onClick={() => console.log(v)}>{1}</span>}
+          selectionChange={(v, a, id) => {
+            console.log(v, id, a)
+          }}
+          onDelete={() => {
+            return new Promise(resolve => {
+              setTimeout(() => {
+                resolve(111)
+              }, 1)
+            })
+          }}
+        >
+          <div style={{ position: 'relative', zIndex: 999 }}>
+            <h1>12312</h1>
+            <h1>12312</h1>
+            <h1>12312</h1>
+          </div>
+        </SelectionCreator>
       </div>
     );
   }
